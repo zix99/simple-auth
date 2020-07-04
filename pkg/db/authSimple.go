34b0c7d28da2cd8b6a2dde4e3e7b414d8882792e
Christopher LaPointe <cmlapointe11@gmail.com>
2020-07-04 12:49:40 -0400

    Minor refactor

--- pkg/db/authSimple.go
+++ pkg/db/authSimple.go
@@ -20,13 +20,15 @@ type accountAuthSimple struct {
 	PasswordBcrypt string `gorm:"not null"`
 }
 
+var ErrorAccountInactive = errors.New("Inactive Account")
+
 // verifyPassword checks a password against the bcrypt entry
 func (s *accountAuthSimple) verifyPassword(against string) bool {
 	return bcrypt.CompareHashAndPassword([]byte(s.PasswordBcrypt), []byte(against)) == nil
 }
 
 // CreateAccountAuthSimple creates a new account simple auth with a crypted password
-func (db *sadb) CreateAccountAuthSimple(belongsTo *Account, username, password string) error {
+func (s *sadb) CreateAccountAuthSimple(belongsTo *Account, username, password string) error {
 	if !belongsTo.Active {
 		return errors.New("Unable to associate with deactivated account")
 	}
@@ -42,17 +44,44 @@ func (db *sadb) CreateAccountAuthSimple(belongsTo *Account, username, password s
 		PasswordBcrypt: string(hashed),
 	}
 
-	db.db.Create(auth)
+	s.db.Create(auth)
 	return nil
 }
 
-func (db *sadb) FindAndVerifySimpleAuth(username, password string) (*Account, error) {
+func (s *sadb) resolveSimpleAuthForUser(username string) (*accountAuthSimple, *Account, error) {
+	if username == "" {
+		return nil, nil, errors.New("Invalid username")
+	}
+
+	var simpleAuth accountAuthSimple
+	if err := s.db.Where(&accountAuthSimple{Username: username}).First(&simpleAuth).Error; err != nil {
+		return nil, nil, err
+	}
+
+	var account Account
+	if err := s.db.Model(&simpleAuth).Related(&account).Error; err != nil {
+		return nil, nil, err
+	}
+
+	if !account.Active {
+		return nil, nil, ErrorAccountInactive
+	}
+
+	return &simpleAuth, &account, nil
+}
+
+func (s *sadb) FindAccountForSimpleAuth(username string) (*Account, error) {
+	_, account, err := s.resolveSimpleAuthForUser(username)
+	return account, err
+}
+
+func (s *sadb) FindAndVerifySimpleAuth(username, password string) (*Account, error) {
 	if username == "" || password == "" {
 		return nil, errors.New("Invalid arg")
 	}
 
-	var auth accountAuthSimple
-	if err := db.db.Where(&accountAuthSimple{Username: username}).First(&auth).Error; err != nil {
+	auth, account, err := s.resolveSimpleAuthForUser(username)
+	if err != nil {
 		return nil, err
 	}
 
@@ -60,36 +89,5 @@ func (db *sadb) FindAndVerifySimpleAuth(username, password string) (*Account, er
 		return nil, UserVerificationFailed
 	}
 
-	var account Account
-	if err := db.db.Model(&auth).Related(&account).Error; err != nil {
-		return nil, UserVerificationFailed
-	}
-
-	if !account.Active {
-		return nil, errors.New("Account Inactive")
-	}
-
-	return &account, nil
-}
-
-func (s *sadb) FindAccountForSimpleAuth(username string) (*Account, error) {
-	if username == "" {
-		return nil, errors.New("Invalid username")
-	}
-
-	var simpleAuth accountAuthSimple
-	if err := s.db.Where(&accountAuthSimple{Username: username}).First(&simpleAuth).Error; err != nil {
-		return nil, err
-	}
-
-	var account Account
-	if err := s.db.Model(&simpleAuth).Related(&account).Error; err != nil {
-		return nil, err
-	}
-
-	if !account.Active {
-		return nil, errors.New("Account Inactive")
-	}
-
-	return &account, nil
+	return account, nil
 }

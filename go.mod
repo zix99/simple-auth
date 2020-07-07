27b0e1d5f4e42e190575163f6bb65adfb7a0bdf2
Christopher LaPointe <cmlapointe11@gmail.com>
2020-07-07 02:39:27 +0000

    Some tests

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.7 // indirect
 	github.com/sirupsen/logrus v1.6.0
+	github.com/stretchr/testify v1.6.1
 	github.com/valyala/fasttemplate v1.1.1 // indirect
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 	golang.org/x/net v0.0.0-20200625001655-4c5254603344 // indirect
---
+++ pkg/config/argparser/argparser_test.go
@@ -0,0 +1,43 @@
+package argparser
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type testNestedArgs struct {
+	Nested string
+}
+
+type testArgs struct {
+	Value      string
+	Number     int
+	unexported int
+	Bool       bool
+	Sub        testNestedArgs
+}
+
+func TestSimpleParsing(t *testing.T) {
+	var obj testArgs
+	err := LoadArgs(&obj, strings.Split("--value=abc --number=123 --bool=true --sub-nested=qef", " ")...)
+	assert.NoError(t, err)
+	assert.Equal(t, obj.Value, "abc")
+	assert.Equal(t, obj.Number, 123)
+	assert.Equal(t, obj.Bool, true)
+	assert.Equal(t, obj.Sub.Nested, "qef")
+	assert.Equal(t, obj.unexported, 0)
+}
+
+func TestSettingUnexportedField(t *testing.T) {
+	var obj testArgs
+	err := LoadArgs(&obj, "--unexported=5")
+	assert.Error(t, err)
+}
+
+func TestBadArgFormat(t *testing.T) {
+	var obj testArgs
+	err := LoadArgs(&obj, "--unexported 5")
+	assert.Error(t, err)
+}

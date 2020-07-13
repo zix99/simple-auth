1867e3d0a61b3c5ecfa5776ef7c7d676af206205
Christopher LaPointe <cmlapointe11@gmail.com>
2020-07-13 02:06:31 +0000

    Skeleton cookie and oidc login

---
+++ cmd/server/loginOidc.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+// Setup router on /oidc
+func setupOidcRouter(group *echo.Group) {
+	group.Group("", routeGetOidc)
+}
+
+func routeGetOidc(c echo.Context) error {
+	return c.HTML(http.StatusOK, "Hi")
+}
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -25,9 +25,9 @@ type ConfigWebRequirements struct {
 
 type ConfigRecaptchaV2 struct {
 	Enabled bool
-	SiteKey string
-	Secret  string
-	Theme   string
+	SiteKey string // Site key, as provided by google
+	Secret  string // Secret for google v2 api (server side call)
+	Theme   string // Themes (dark or light)
 }
 
 type ConfigJWT struct {
@@ -36,11 +36,36 @@ type ConfigJWT struct {
 	ExpiresMinutes int
 }
 
+type ConfigLogin struct {
+	// SameDomain authentication uses a cookie set to a domain (and presumably shared with your site).  Easiest to implement in a full-trust environment
+	Cookie struct {
+		Enabled        bool
+		Name           string // Name of the cookie
+		SigningKey     string // Key used to sign cookie (and later for you to verify!)
+		ExpiresMinutes int
+		Path           string
+		Domain         string
+		SecureOnly     bool
+		HTTPOnly       bool
+	}
+	// OIDC (OAuth 2) flow that allows an external site to securely login and verify the user
+	OIDC struct {
+		Enabled bool
+		Clients map[string]struct { // ClientID Key
+			Name        string
+			Author      string
+			ClientID    string
+			RedirectURI []string
+		}
+	}
+}
+
 type ConfigWeb struct {
 	Host         string
 	Requirements ConfigWebRequirements
 	RecaptchaV2  ConfigRecaptchaV2
 	JWT          ConfigJWT
+	Login        ConfigLogin
 	Metadata     map[string]interface{}
 }
 
@@ -55,8 +80,8 @@ type ConfigEmail struct {
 
 type Config struct {
 	Db             ConfigDatabase
-	Web            ConfigWeb
-	Email          ConfigEmail
-	Authenticators ConfigAuthencatorSet
-	Production     bool
+	Web            ConfigWeb            // Configure how the user interacts with the web
+	Email          ConfigEmail          // SMTP/Email sending config
+	Authenticators ConfigAuthencatorSet // Describes API Authenticators
+	Production     bool                 // Production changes how logs are generated and tighter security checks
 }

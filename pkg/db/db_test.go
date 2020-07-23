a4fc0d3b4576f10f588087623f031e8dc54038f3
Christopher LaPointe <cmlapointe11@gmail.com>
2020-07-23 18:11:37 -0400

    Move db tests

--- pkg/db/db_test.go
+++ pkg/db/db_test.go
@@ -1,6 +1,11 @@
 package db_test
 
-import "simple-auth/pkg/db"
+import (
+	"simple-auth/pkg/db"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 var sadb db.SADB
 
@@ -8,4 +13,8 @@ func init() {
 	sadb = db.New("sqlite3", "file::memory:?cache=shared")
 	createAuthSimpleMock()
 	createAuthTokenMock()
+}
+
+func TestIsAlive(t *testing.T) {
+	assert.True(t, sadb.IsAlive())
 }

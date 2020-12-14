75161609994dfce6b9c4505f64e31f7c2132a04b
Christopher LaPointe <cmlapointe11@gmail.com>
2020-12-14 03:02:05 +0000

    Remove dead code

--- pkg/routes/api/ui/errorCodes.go
+++
@@ -1,9 +0,0 @@
-package ui
-
-import "simple-auth/pkg/saerrors"
-
-const (
-	// Common
-	errorInvalidAccount saerrors.ErrorCode = "invalid-account"
-	errorEmailSend      saerrors.ErrorCode = "email-send"
-)

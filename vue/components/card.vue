f3f7acc74912aa97d0b2f7fd9e6b49fef4312a6c
Christopher LaPointe <cmlapointe11@gmail.com>
2021-02-15 20:32:15 +0000

    Better 404 page

--- vue/components/card.vue
+++ vue/components/card.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="card my-3">
-        <header class="card-header">
+        <header class="card-header" v-if="title">
             <p class="card-header-title">{{title}}</p>
         </header>
         <div class="card-content">
--- vue/routes/pageNotFound.vue
+++ vue/routes/pageNotFound.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="columns is-centered">
-    <div class="column is-half">
-      <h2 class="title is-2">Page Not Found</h2>
-      <p>The page you were looking for could not be found.</p>
-    </div>
-  </div>
+  <CenterCard title="Error">
+    <h2 class="title is-2">Page Not Found</h2>
+    <p>
+      The page you were looking for could not be found. <router-link to="/">Go Home</router-link>
+    </p>
+  </CenterCard>
 </template>
+
+<script>
+import CenterCard from '../components/centerCard.vue';
+
+export default {
+  components: {
+    CenterCard,
+  },
+};
+</script>

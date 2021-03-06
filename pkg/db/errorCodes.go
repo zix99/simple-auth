d0d0835194f83e79974622ace41eecdc521289be
Christopher LaPointe <cmlapointe11@gmail.com>
2021-03-06 14:14:47 +0000

    Remove dead code

--- pkg/db/errorCodes.go
+++ pkg/db/errorCodes.go
@@ -17,12 +17,8 @@ const (
 	AuthInvalidUsername saerrors.ErrorCode = "invalid-username"
 
 	// authToken
-	SessionInvalidCredentials saerrors.ErrorCode = "invalid-credentials"
-	SessionNotFound           saerrors.ErrorCode = "session-missing"
-	SessionInvalidated        saerrors.ErrorCode = "session-invalid"
-	SessionExpired            saerrors.ErrorCode = "session-expired"
-	VerificationMissing       saerrors.ErrorCode = "verification-missing"
-	VerificationConsumed      saerrors.ErrorCode = "verification-consumed"
-	VerificationExpired       saerrors.ErrorCode = "verification-expired"
-	VerificationInvalid       saerrors.ErrorCode = "verification-invalid"
+	VerificationMissing  saerrors.ErrorCode = "verification-missing"
+	VerificationConsumed saerrors.ErrorCode = "verification-consumed"
+	VerificationExpired  saerrors.ErrorCode = "verification-expired"
+	VerificationInvalid  saerrors.ErrorCode = "verification-invalid"
 )
